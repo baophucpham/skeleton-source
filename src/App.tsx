afa4b2dfa3918efa9a956c6ceaf6aeb21031a428
baophucpham <baophucpham74@gmail.com>
2024-08-27 00:06:52 +0700

    config new router

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/loginPage';
 import { ConnectedRouter } from 'connected-react-router';
+import routes from './router/router';
 
 interface Props {
     history: any;
@@ -11,7 +12,17 @@ const App: React.FC<Props> = ({ history }: Props) => {
     return (
         <ConnectedRouter history={history}>
             <Switch>
-                <Route exact path="" component={Login} />
+                {routes.length > 0 &&
+                    routes.map(({routes, index}:any) => {
+                        return (
+                            <Route
+                                key={index}
+                                path={routes.path}
+                                exact={routes.exact}
+                                component={routes.main}
+                            />
+                        );
+                    })}
                 {/* <PrivateRoute path="/admin" component={Admin} isAuthenticated={isAuthenticated} /> */}
                 <Route path="*">{/* <NotFound /> */}</Route>
             </Switch>
---
+++ src/router/router.tsx
@@ -0,0 +1,17 @@
+import Login from 'src/pages/loginPage';
+import PrivatePage from 'src/pages/privatePage';
+
+const routes = [
+    {
+        path: '/',
+        exact: true,
+        main: () => <Login />,
+    },
+    {
+        path: '/',
+        exact: true,
+        main: () => <PrivatePage />,
+    },
+];
+
+export default routes;
