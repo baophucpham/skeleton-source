72f840805b433c08b7e912f20a44db3b1ee32951
baophucpham <baophucpham74@gmail.com>
2024-08-29 01:14:14 +0700

    new update

--- src/pages/InformationPage/IntroductionGolfTravel/index.tsx
+++ src/pages/InformationPage/IntroductionGolfTravel/index.tsx
@@ -1,18 +1,46 @@
 import React from 'react';
-import InformationPaymentStyle from './style';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faCircleUser,
-    faDownload,
-    faPhoneFlip,
-    faShareNodes,
-} from '@fortawesome/free-solid-svg-icons';
-import { Button } from 'antd';
-import icons from 'src/asset/icon';
 import IntroductionGoflTravelStyle from './style';
 import images from 'src/asset/image';
+import icons from 'src/asset/icon';
+import { Carousel } from 'antd';
 
 const IntroductionGoflTravel = () => {
+    const arrWhyContent = [
+        {
+            id: 1,
+            icon: `${icons.golfTravelExpert}`,
+            title: 'Golf Travel Experts',
+            discription:
+                'A dedicated expert in your chosen destination as standard',
+        },
+        {
+            id: 2,
+            icon: `${icons.bestPriceGuaranteed}`,
+            title: 'Best Price Guaranteed',
+            discription:
+                'Value you can trust and payment options you can rely on.',
+        },
+        {
+            id: 3,
+            icon: `${icons.unbeatableChoice}`,
+            title: 'Unbeatable Choice',
+            discription: '3500+ destinations in 24 countries chosen for you.',
+        },
+        {
+            id: 4,
+            icon: `${icons.industryLeadingProtection}`,
+            title: 'Industry leading Protection ',
+            discription: 'ABTOT & ATOL safeguards for greater peace of mind.',
+        },
+    ];
+    const contentStyle: React.CSSProperties = {
+        margin: 0,
+        height: '160px',
+        color: 'black',
+        lineHeight: '160px',
+        textAlign: 'center',
+        background: '#fff',
+    };
     return (
         <IntroductionGoflTravelStyle>
             <img alt="" className="imgGoflTravel" src={images.golfView} />
@@ -27,14 +55,94 @@ const IntroductionGoflTravel = () => {
                     adjustments. I look forward to assisting you in creating an
                     unforgettable golf getaway.
                 </div>
-                <div className='signIn'>
+                <div className="signIn">
                     <div>Oliver</div>
-                    <div className='inforSeller'>
+                    <div className="inforSeller">
                         <div>Oliver Gunning</div>
                         <div>0207 336 5340 </div>
-                        <div className='mailSeller'> oliverg@yourgolftravel.com</div>
+                        <div className="mailSeller">
+                            {' '}
+                            oliverg@yourgolftravel.com
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div className="viewWhyContent">
+                <div className="titleWhyContent">
+                    Why book with Your Golf Travel?
+                </div>
+                <div className="showItemWhy">
+                    {arrWhyContent.map((item, index) => {
+                        return (
+                            <div className="groupWhyItem" key={index}>
+                                <img
+                                    alt=""
+                                    className="iconWhy"
+                                    src={item.icon}
+                                />
+                                <p className="titleItem">{item.title}</p>
+                                <p className="discriptionItem">
+                                    {item.discription}
+                                </p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className="carouselView">
+                <div className="titleWhyContent">What our golfers say</div>
+                <Carousel
+                    arrows
+                    infinite={false}
+                    nextArrow={
+                        <img
+                            alt=""
+                            className="arrowStep"
+                            style={{ fontSize: '32.987px' }}
+                            src={icons.rightArrow}
+                        />
+                    }
+                    prevArrow={
+                        <img
+                            alt=""
+                            className="arrowStep"
+                            style={{ fontSize: '32.987px' }}
+                            src={icons.leftArrow}
+                        />
+                    }
+                >
+                    <div>
+                        <h3 style={contentStyle}>1</h3>
+                    </div>
+                    <div>
+                        <h3 style={contentStyle}>2</h3>
+                    </div>
+                    <div>
+                        <h3 style={contentStyle}>3</h3>
+                    </div>
+                    <div>
+                        <h3 style={contentStyle}>4</h3>
+                    </div>
+                </Carousel>
+            </div>
+            <div className="carouselView">
+                <div className="titleWhyContent">Next steps</div>
+                <div className="showItemStep">
+                    {arrWhyContent.map((item, index) => {
+                        return (
+                            <div className="groupWhyItem" key={index}>
+                                <img
+                                    alt=""
+                                    className="iconWhy"
+                                    src={item.icon}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className="endView">
+            <div className="titleWhyContent">About <span className='nameSeller'>Oliver Gunning </span> - Your UK & Ireland Golf Travel Expert</div>
             </div>
         </IntroductionGoflTravelStyle>
     );
--- src/pages/InformationPage/IntroductionGolfTravel/style.ts
+++ src/pages/InformationPage/IntroductionGolfTravel/style.ts
@@ -17,22 +17,79 @@ const IntroductionGoflTravelStyle = styled('div')`
         font-weight: 400;
         line-height: 20px;
         color: #4f4f4f;
+        margin-bottom: 37px;
     }
-    .content{
+    .content {
         width: 80%;
     }
-    .signIn{
+    .signIn {
         width: 100%;
         display: flex;
         justify-content: space-between;
         margin-top: 20px;
-        font-family: "The Nautigal";
+        font-family: 'The Nautigal';
     }
-    .inforSeller{
+    .inforSeller {
         text-align: right;
     }
-    .mailSeller{
+    .mailSeller {
         text-decoration-line: underline;
+        color: #12b897;
+    }
+    .viewWhyContent {
+        width: 100%;
+    }
+    .titleWhyContent {
+        font-size: 23.758px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 28.125px;
+    }
+    .showItemWhy {
+        display: flex;
+        margin-top: 35px;
+        justify-content: space-between;
+        padding-bottom: 42px;
+        border-bottom: 1px solid #dcdcdc;
+    }
+    .groupWhyItem {
+        width: 20%;
+    }
+    .iconWhy {
+        margin-bottom: 17px;
+    }
+    .titleItem {
+        font-size: 16.071px;
+        font-weight: 700;
+        line-height: 22.098px;
+        color: #4f4f4f;
+    }
+    .discriptionItem {
+        font-size: 16.071px;
+        font-weight: 400;
+        line-height: 22.098px;
+        color: #4f4f4f;
+    }
+    .slick-arrow .slick-next {
+        width: 32.987px !important;
+        height: 34.152px !important;
+    }
+    .carouselView{
+        margin-top: 23px;
+        border-bottom: 1px solid #dcdcdc;
+        padding-bottom: 48px;
+    }
+    .showItemStep{
+        display: flex;
+        margin-top: 35px;
+        justify-content: space-between;
+        padding-bottom: 42px;
+    }
+    .endView{
+        margin-top: 23px;
+        padding-bottom: 48px;
+    }
+    .nameSeller{
         color: #12B897;
     }
 `;
