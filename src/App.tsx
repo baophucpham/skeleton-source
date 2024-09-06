0ef656f2a82a018cd96a7bdee85509d0f28b1204
baophucpham <baophucpham74@gmail.com>
2024-09-06 15:49:25 +0700

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import ProtectedRoute from './router/protectedRoutes';
 import { useSelector } from 'react-redux';
